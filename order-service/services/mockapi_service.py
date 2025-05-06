6605ba3a13fe4dbc9d1a20b16caf4fee13bae42b
Sandy-1711 <sandy1711003@gmail.com>
2025-05-06 20:53:20 +0530

    Added mockapi_service that calls mockapis

---
+++ order-service/services/mockapi_service.py
@@ -0,0 +1,11 @@
+class MockAPI:
+    """Service that calls the mockapi and returns the responses"""
+    
+    def __init__(self) -> None:
+        pass
+
+    def call_mock_api(self, endpoint, analysis_data):
+        pass
+        
+    def get_mockapi_service(self):
+        return self
