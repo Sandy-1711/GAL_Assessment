34f8feb07fdd1a7894eaf6c52b333061946b4ba7
Sandy-1711 <sandy1711003@gmail.com>
2025-05-06 20:55:14 +0530

    Fixed parameters for MockAPI service and added implementation in order_service.py

--- order-service/services/mockapi_service.py
+++ order-service/services/mockapi_service.py
@@ -1,10 +1,10 @@
 class MockAPI:
     """Service that calls the mockapi and returns the responses"""
-    
+
     def __init__(self) -> None:
         pass
 
-    def call_mock_api(self, endpoint, analysis_data):
+    def call_mock_api(self, endpoint, parameters):
         pass
         
     def get_mockapi_service(self):
--- order-service/services/order_service.py
+++ order-service/services/order_service.py
@@ -7,14 +7,15 @@ from datetime import datetime
 from fastapi import HTTPException
 from .llm_service import LLMService
 from .prompt_helper_service import PromptHelperService
-
+from .mockapi_service import MockAPI
 
 class OrderService:
     def __init__(self) -> None:
         self.llm = LLMService().get_llm()
         self.order_query_analysis_prompt = PromptHelperService().get_order_query_analysis_prompt
         self.response_formatting_prompt = PromptHelperService().get_respose_formatting_prompt()
-    
+        self.mockapi_service = MockAPI.get_mockapi_service()
+
     def process_order_query(self, customer_id, user_query):
         try:
 
@@ -31,7 +32,7 @@ class OrderService:
             except json.JSONDecodeError:
                 return f"Failed to parse query analysis output: {analysis_result.content}"
 
-            
+                  
 
         except Exception as e:
             return HTTPException(status_code=500, detail=f"Error handling order query: {str(e)}")
