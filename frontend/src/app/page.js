0e5412dc75cf63332a57140cc00be940f37e87d7
Sandy-1711 <sandy1711003@gmail.com>
2025-05-07 21:15:14 +0530

    Made the ui for input box and created ChatMessagesBox component to show messages

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -3,11 +3,14 @@ import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { addMessage, clear } from "../../redux/features/chatSlice";
 import InputBox from "@/components/InputBox";
+import ChatMessagesBox from "@/components/ChatMessagesBox";
 
 export default function Home() {
   const dispatch = useDispatch();
-  const chat = useSelector(state => state.chat).value
-  return <div className="h-screen w-screen bg-background">
+  const chat = useSelector(state => state.chat).value;
+  let lengthOfMessages = chat?.messages?.length;
+  return <div className="h-screen w-screen bg-background flex justify-center items-center">
+    {lengthOfMessages > 0 && <ChatMessagesBox messages={chat.messages} />}
     <InputBox numberOfMessages={chat?.messages?.length} />
   </div>
 }
--- frontend/src/components/InputBox.js
+++ frontend/src/components/InputBox.js
@@ -1,3 +1,19 @@
-export default function InputBox() { 
-    return
+'use client'
+import { ArrowUp } from "lucide-react";
+import { useState } from "react";
+
+export default function InputBox() {
+
+    const [text, setText] = useState("");
+    
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+    }
+    
+    return <form onSubmit={handleFormSubmit} className="bg-foreground overflow-hidden px-3 pl-7 py-3 w-1/2 flex justify-between items-center rounded-3xl">
+        <input value={text} o onChange={(e) => { setText(e.target.value) }} className="bg-foreground h-10 outline-none flex-1 text-white" placeholder="Ask anything..." />
+        <button className="bg-white h-9 w-9 rounded-full flex justify-center hover:bg-gray-400 transition-colors items-center">
+            <ArrowUp className="" size={22} />
+        </button>
+    </form>
 }
