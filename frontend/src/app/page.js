2b4df260c5763bc0a515def7d611455fa8afeca5
Sandy-1711 <sandy1711003@gmail.com>
2025-05-07 21:01:47 +0530

    Created a InputBox component under components/ui

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,11 +2,12 @@
 import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { addMessage, clear } from "../../redux/features/chatSlice";
+import InputBox from "@/components/InputBox";
 
 export default function Home() {
   const dispatch = useDispatch();
   const chat = useSelector(state => state.chat).value
   return <div className="h-screen w-screen bg-background">
-    
+    <InputBox numberOfMessages={chat?.messages?.length} />
   </div>
 }
