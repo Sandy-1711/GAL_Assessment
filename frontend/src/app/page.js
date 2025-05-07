5f90e20491a8977a48f7446b9f3c92a16234f197
Sandy-1711 <sandy1711003@gmail.com>
2025-05-07 20:55:06 +0530

    Wrapped layout.js with provider and added implementation of redux state in page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,10 @@
+'use client'
+import { useSelector } from "react-redux"
 export default function Home() {
+
+  const chat = useSelector(state => state.chat).value
+  console.log(chat.messages)
   return <div className="h-screen w-screen bg-white">
-    
+
   </div>
 }
