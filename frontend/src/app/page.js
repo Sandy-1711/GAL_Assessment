85039ba6a1749a0728fb10ad344179cc8ebad5fa
Sandy-1711 <sandy1711003@gmail.com>
2025-05-07 21:00:06 +0530

    Changed the color scheme to match that of chatgpt

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { addMessage, clear } from "../../redux/features/chatSlice";
+
 export default function Home() {
-
+  const dispatch = useDispatch();
   const chat = useSelector(state => state.chat).value
-  console.log(chat.messages)
-  return <div className="h-screen w-screen bg-white">
-
+  return <div className="h-screen w-screen bg-background">
+    
   </div>
 }
